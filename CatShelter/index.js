7f6e0c56f7e6b0fcd17458bf613d1031c6ad78f6
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-09-14 22:49:17 +0300

    update

--- CatShelter/index.js
+++ CatShelter/index.js
@@ -1,9 +1,16 @@
 const http = require("http");
+const homeTemplate = require('./views/home/index');
+//const { log } = require("console");
 const PORT = 5000
 
 
 
 const server = http.createServer((req, res) =>{
+    const {url} = req;
+    console.log(url);
+    if(url === '/'){
+        console.log(homeTemplate);
+    }
 
     res.write('Hellow Update');
     res.end();
