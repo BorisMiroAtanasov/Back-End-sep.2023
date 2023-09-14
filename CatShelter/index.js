6c5f9d565168801c6e44aeef5f0413b11a7fbf55
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-09-14 23:38:56 +0300

    update cat shelter

--- CatShelter/index.js
+++ CatShelter/index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const homeHtml = require("./views/home/index.js");
 const siteCss = require("./content/styles/site.js");
 const addBreedHtml = require("./views/addBreed.js")
+const addCatHtml = require("./views/addCat.js")
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
@@ -24,6 +25,11 @@ const server = http.createServer((req, res) => {
         "Content-type": "text/html",
       });
     res.write(addBreedHtml);
+  } else if( url === '/cats/add-cat'){
+    res.writeHead(200, {
+        "Content-type": "text/html",
+      });
+    res.write(addCatHtml);
   }
 
   res.write("Hellow Update");
