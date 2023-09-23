d41ec960115def18736870d38a811c3c3530fd33
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-09-23 16:03:15 +0300

    cat shalter v3 update

--- CatShelter_v3/server.js
+++ CatShelter_v3/server.js
@@ -2,6 +2,16 @@ const http = require('http');
 const port = 5002;
 
 
-const server= http.createServer((res, req), {
-    
+const server= http.createServer((req, res) => {
+   res.writeHead(200,{
+    "Content-Type": "text/plain"
+   });
+
+   res.write("Hellow JS WORD");
+   res.end()
+
+
 })
+
+
+server.listen(port, () => console.log(`server is listen on port ${port}`));
