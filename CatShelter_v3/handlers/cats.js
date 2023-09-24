47ed47c82de85ac6b65d5c8f8d1c0dca06ef0188
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-09-25 00:04:24 +0300

    cat shelter 3 update

--- CatShelter_v3/handlers/cats.js
+++ CatShelter_v3/handlers/cats.js
@@ -11,11 +11,11 @@ const cats = require('../data/cats.json');
 
 module.exports = (req, res) => {
     const pathName = url.parse(req.url).pathname;
-    
 
-    if(pathName === '/cats/add-cat' && req.method === "GET"){
+
+    if (pathName === '/cats/add-cat' && req.method === "GET") {
         let filePath = path.normalize(
-            path.join(__dirname,"../views/addCat.html")
+            path.join(__dirname, "../views/addCat.html")
         );
 
         const index = fs.createReadStream(filePath);
@@ -34,18 +34,18 @@ module.exports = (req, res) => {
         index.on('error', (err) => {
             console.log(err)
         });
-    
 
-    } else if(pathName === '/cats/add-breed' && req.method === "GET"){
+
+    } else if (pathName === '/cats/add-breed' && req.method === "GET") {
         let filePath = path.normalize(
-            path.join(__dirname,"../views/addBreed.html")
+            path.join(__dirname, "../views/addBreed.html")
 
 
         );
         const index = fs.createReadStream(filePath);
 
         index.on('data', (data) => {
-           
+
             res.write(data)
         })
 
@@ -58,7 +58,7 @@ module.exports = (req, res) => {
         })
     } else if (pathName === '/cats/add-breed' && req.method === 'POST') {
         let formData = '';
-        
+
 
         req.on('data', (data) => {
             formData += data;
@@ -67,29 +67,29 @@ module.exports = (req, res) => {
         req.on('end', () => {
             let body = qs.parse(formData);
 
-            fs.readFile('./data/breeds.json', (err, data) =>{
+            fs.readFile('./data/breeds.json', (err, data) => {
                 if (err) {
                     throw err;
                 }
 
                 //console.log(data)
-              
+
 
                 let breeds = JSON.parse(data);
 
                 //console.log(breeds)
                 breeds.push(body.breed);
-               // console.log(breeds)
+                // console.log(breeds)
                 let json = JSON.stringify(breeds);
-               // console.log(json)
+                // console.log(json)
 
 
                 fs.writeFile("./data/breeds.json", json, "utf-8", () => console.log('Your breed was added successfully!'));
 
-                res.writeHead(303, {location: '/'})
+                res.writeHead(303, { location: '/' })
                 res.end();
             });
-    
+
         });
 
 
@@ -123,13 +123,54 @@ module.exports = (req, res) => {
     //     })
 
     //}
+    else if (pathName.includes('/edit') && req.method === 'GET') {
+        let filepath = path.normalize(path.join(__dirname, '../views/editCat.html'))
+
+        const index = fs.createReadStream(filepath);
+
+        index.on('data', (data) => {
+            let parts = pathName.split('/');
+            //let catId = Number(parts[parts.length - 1])
+            let catId = parts[2]
+            console.log(parts)
+            console.log(catId)
+            //console.log(data.toString())
+            // const findCatById = (catId) => {
+            //     const [key, cat] = Object.entries(cats).find(([key, cat]) => cat.id === catId);
+            //     return cat;
+            // }
+            let currentCat = cats.find((x) => x.id == catId);
+            //const currentCat = findCatById(catId);
+            console.log(currentCat)
+            let modifiedData = data.toString().replace('{{id}}', catId)
+            modifiedData = modifiedData.replace('{{name}}', currentCat.name)
+            modifiedData = modifiedData.replace('{{description}}', currentCat.description)
+
+            const breedsAsOptions = breeds.map((b) => `<option value="${b}">${b}</option>`)
+            modifiedData = modifiedData.replace('{{catBreeds}}', breedsAsOptions.join('/'))
+           console.log(modifiedData)
+            //modifiedData = modifiedData.replace('{{breed}}', currentCat.breed)
+            res.write(modifiedData)
+           // res.end()
+        })
+
+
+        index.on('end', () => {
+            res.end()
+        })
+
+        index.on('error', (err) => {
+            console.log(err)
+        })
+
+
+    }
     else {
         return true;
     }
-    
 
-    
-    
-    
-    
+
+
+
+
 }
