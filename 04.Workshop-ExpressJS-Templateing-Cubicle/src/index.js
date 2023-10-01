ecc612d4e9af976a0e6d2a157b7e636630298ddb
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-10-02 00:00:34 +0300

    CUB-P1 del not needed footers and headers

--- 04.Workshop-ExpressJS-Templateing-Cubicle/src/index.js
+++ 04.Workshop-ExpressJS-Templateing-Cubicle/src/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
-
-const handlebars = require("express-handlebars")
-const PORT = 5050
+const path = require("path");
+const handlebars = require("express-handlebars");
+const PORT = 5050;
 
 // Handlebars configuration
-app.engine("hbs", handlebars.engine({extname: "hbs"}));
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
-app.set("views", "src/views")
+app.set("views", "src/views");
+
+//setup static files
+const staticFile = express.static(path.resolve(__dirname, "public"));
+app.use(staticFile);
 
 app.get("/", (req, res) => {
-    //res.send("Hello from express server!")
-    res.render("index")
+  //res.send("Hello from express server!")
+  res.render("index");
 });
 
-
-
-
-
-app.listen(PORT, () => console.log(`Server is running on port : ${PORT}...`))
+app.listen(PORT, () => console.log(`Server is running on port : ${PORT}...`));
