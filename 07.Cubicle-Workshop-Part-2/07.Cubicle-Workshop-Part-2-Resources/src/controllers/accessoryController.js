19f58d77c2bb4cb65aa99d113af8a5d658331877
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-10-10 23:28:19 +0300

    CUB-P2 add accesssory endpoint and model

--- 07.Cubicle-Workshop-Part-2/07.Cubicle-Workshop-Part-2-Resources/src/controllers/accessoryController.js
+++ 07.Cubicle-Workshop-Part-2/07.Cubicle-Workshop-Part-2-Resources/src/controllers/accessoryController.js
@@ -7,10 +7,13 @@ router.get("/create", (req , res) => {
 res.render("accessory/create")
 });
 
-router.post("/create", (req, res) =>{
-    const { name , description , imageUrl} = req.body;
+router.post("/create", async(req, res) =>{
+     const { name , description , imageUrl} = req.body;
 
-    accessoryService.create({name , description , imageUrl})
+     await accessoryService.create({name , description , imageUrl});
+
+   res.redirect('/');
+   
 })
 
 module.exports = router;
