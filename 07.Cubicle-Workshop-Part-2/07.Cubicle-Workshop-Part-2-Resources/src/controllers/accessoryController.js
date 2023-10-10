7677805d57065b724edfce92372162c513d33b52
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-10-10 19:41:10 +0300

    CUB-P2 update

--- 07.Cubicle-Workshop-Part-2/07.Cubicle-Workshop-Part-2-Resources/src/controllers/accessoryController.js
+++ 07.Cubicle-Workshop-Part-2/07.Cubicle-Workshop-Part-2-Resources/src/controllers/accessoryController.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
+const accessoryService = require('./../service/accessoryService');
 
 
 
 router.get("/create", (req , res) => {
 res.render("accessory/create")
 });
+
+router.post("/create", (req, res) =>{
+    const { name , description , imageUrl} = req.body;
+
+    accessoryService.create({name , description , imageUrl})
+})
 
 module.exports = router;
 
