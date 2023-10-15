1a2580816b3080badacebd38c8feb0738bbfcdeb
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-10-15 14:09:00 +0300

    CUB-P3 - login , register controller

--- 10.Sessions-and-Authentication-Workshop-Resources/Cubicle-Workshop-Part-3_authentication/src/router.js
+++ 10.Sessions-and-Authentication-Workshop-Resources/Cubicle-Workshop-Part-3_authentication/src/router.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const homeController = require("./controllers/homeController.js");
 const cubeController = require("./controllers/cubeController.js");
 const accessoryController = require("./controllers/accessoryController.js")
+const usersController = require("./controllers/userController.js")
 
 router.use(homeController);
 router.use("/cubes", cubeController);
 router.use("/accessory", accessoryController)
+router.use("/users", usersController)
 
 router.get("*", (req, res) => {
   res.redirect("/404");
