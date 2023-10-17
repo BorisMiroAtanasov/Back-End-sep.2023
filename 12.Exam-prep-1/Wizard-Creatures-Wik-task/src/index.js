f25d10c4aaa3e340240a6ef8c8ea353c172d0636
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-10-17 19:40:24 +0300

    Exam-prep WC update

--- 12.Exam-prep-1/Wizard-Creatures-Wik-task/src/index.js
+++ 12.Exam-prep-1/Wizard-Creatures-Wik-task/src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const app = express();
+const path = require('path')
 const {PORT} = require('./constants');
 
+
+//Init
+const app = express();
+
+//Express congigurations
+app.use(express.static(path.resolve(__dirname, "./public")))
 app.use(express.urlencoded({extended: false}))
  
+
+//Routes
 app.get("/" , (req, res) =>{
     res.send("hello home page!")
 });
