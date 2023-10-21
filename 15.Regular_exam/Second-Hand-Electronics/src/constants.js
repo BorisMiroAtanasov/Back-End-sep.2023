d1a44c78f6ff37a30d3dafd10e15aa02d78d7c57
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-10-21 14:59:30 +0300

    RE-resister, login and logout functionality done

---
+++ 15.Regular_exam/Second-Hand-Electronics/src/constants.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    PORT: 3000,
+    DB_ULR: "mongodb://127.0.0.1:27017/second-hand-electronics",  // localhost = 127.0.0.1,
+    SECRET: "ELECTRONICS_SECRET",
+};
+
+module.exports = CONFIG;
--- 15.Regular_exam/Wizard-Creatures-Wik-task_part 2/src/constants.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    PORT: 5050,
-    DB_ULR: "mongodb://127.0.0.1:27017/wizrd-creatures",  // localhost = 127.0.0.1,
-    SECRET: "BORIS_SECRET",
-};
-
-module.exports = CONFIG;
