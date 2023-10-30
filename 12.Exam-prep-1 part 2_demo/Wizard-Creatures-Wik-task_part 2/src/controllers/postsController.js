4829012939b6f9654109a797cd74cab872323dcc
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-10-31 00:20:14 +0200

    exam prep WC - implement create functionality

--- 12.Exam-prep-1 part 2_demo/Wizard-Creatures-Wik-task_part 2/src/controllers/postsController.js
+++ 12.Exam-prep-1 part 2_demo/Wizard-Creatures-Wik-task_part 2/src/controllers/postsController.js
@@ -1,19 +1,24 @@
-const router  = require("express").Router();
+const router = require("express").Router();
+const creatureServise = require ('../services/creatureServise')
 
-router.get('/all', (req, res) =>{
-   res.render("post/all-posts")
-
+router.get("/all", (req, res) => {
+  res.render("post/all-posts");
 });
 
+router.get("/create", (req, res) => {
+  res.render("post/create");
+});
+router.post("/create", async(req, res) => {
+   const { name, species, skinColor, eyeColor, image, description } = req.body;
+   const payload = { name, species, skinColor, eyeColor, image, description }
+   
 
-router.get('/create', (req, res) =>{
-   res.render("post/create")
-
+   await creatureServise.create(payload)
+  res.redirect("/posts/all");
 });
 
-router.get('/profile', (req, res) =>{
-   res.render("post/profile")
+router.get("/profile", (req, res) => {
+  res.render("post/profile");
+});
 
-})
-
-module.exports = router
+module.exports = router;
