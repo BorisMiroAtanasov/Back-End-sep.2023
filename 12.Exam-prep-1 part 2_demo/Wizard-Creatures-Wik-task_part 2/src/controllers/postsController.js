dd6c1a3a7b3f9c32aafb0551cb7526374c5077aa
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-11-01 23:00:59 +0200

    exam prep WC - implement details page

--- 12.Exam-prep-1 part 2_demo/Wizard-Creatures-Wik-task_part 2/src/controllers/postsController.js
+++ 12.Exam-prep-1 part 2_demo/Wizard-Creatures-Wik-task_part 2/src/controllers/postsController.js
@@ -1,16 +1,19 @@
 const router = require("express").Router();
 const creatureServise = require ('../services/creatureServise')
 
-router.get("/all", (req, res) => {
-  res.render("post/all-posts");
+router.get("/all", async(req, res) => {
+   const cretures =  await creatureServise.getAll().lean()     // или подаваме [] -> there is no creatures
+   //console.log({cretures});
+  res.render("post/all-posts", {cretures});
 });
 
 router.get("/create", (req, res) => {
   res.render("post/create");
 });
 router.post("/create", async(req, res) => {
+   //console.log(req);
    const { name, species, skinColor, eyeColor, image, description } = req.body;
-   const payload = { name, species, skinColor, eyeColor, image, description }
+   const payload = { name, species, skinColor, eyeColor, image, description, owner: req.user }
    
 
    await creatureServise.create(payload)
@@ -20,5 +23,15 @@ router.post("/create", async(req, res) => {
 router.get("/profile", (req, res) => {
   res.render("post/profile");
 });
+
+router.get("/:creatureId/details" , async(req,res) => {
+   const {creatureId} = req.params
+  // console.log({creatureId});
+
+   const creature = await creatureServise.singleCreature(creatureId).lean()
+   
+   //console.log({creature});
+   res.render("post/details" , {creature})
+})
 
 module.exports = router;
--- 12.Exam-prep-1 part 2_demo/Wizard-Creatures-Wik-task_part 2/src/services/creatureServise.js
+++ 12.Exam-prep-1 part 2_demo/Wizard-Creatures-Wik-task_part 2/src/services/creatureServise.js
@@ -2,3 +2,9 @@ const Creature = require('../models/Creature')
 
 
 exports.create = (createData) => Creature.create(createData)
+
+exports.getAll = () => Creature.find()
+
+exports.singleCreature = (creatureId) => Creature.findById(creatureId)
+
+
