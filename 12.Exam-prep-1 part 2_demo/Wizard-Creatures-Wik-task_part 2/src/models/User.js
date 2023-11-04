b02beccb7a2c12d0d1ec448e80ce0e4bb55b78a6
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-11-04 14:00:12 +0200

    Exam prep Validating - min chars - done

--- 12.Exam-prep-1 part 2_demo/Wizard-Creatures-Wik-task_part 2/src/models/User.js
+++ 12.Exam-prep-1 part 2_demo/Wizard-Creatures-Wik-task_part 2/src/models/User.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-
-    firstName:{type: String, required: true},
-    lastName:{type: String, required: true},
-    email: {type: String, required: true, unique :true},
-    password: {type: String, required: true},
+  firstName: { type: String, required: true, minLength: 3 },
+  lastName: { type: String, required: true ,minLength: 3 },
+  email: { type: String, required: true, unique: true , minLength: 10},
+  password: { type: String, required: true , minLength: 4},
 });
 
 // userSchema.path('email').validate(function (emailInput) {
@@ -16,19 +13,18 @@ const userSchema = new mongoose.Schema({
 //     return !!email
 // }, "Email already exists!")
 
-userSchema.virtual('repeatPassword').set(function (value) {
-    // console.log(value);
-    // console.log(this.password);
-if(value !== this.password){
-    throw new Error("Password missmatch")
-}
-
+userSchema.virtual("repeatPassword").set(function (value) {
+  // console.log(value);
+  // console.log(this.password);
+  if (value !== this.password) {
+    throw new Error("Password missmatch");
+  }
 });
 
-userSchema.pre("save", async function(){
-    const hash = await bcrypt.hash(this.password, 10);
-    this.password = hash
-})
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+});
 
 const User = mongoose.model("User", userSchema);
 
