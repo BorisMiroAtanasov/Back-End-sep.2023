06c40e272614f18c8f73aa5623b0d5937f618f7e
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-11-07 00:03:02 +0200

    organize folders

--- 06.Workshop-ExpressJS-Templateing-Cubicle/03.Cubicle-Workshop-Part-1_IP_2/src/index.js
+++ 06.Workshop-ExpressJS-Templateing-Cubicle/03.Cubicle-Workshop-Part-1_IP_2/src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const handlebaars = require('express-handlebars');
 const PORT = 5000;
 
 const app = express();
 
+
+// handlebars config
+app.engine('hbs', handlebaars.engine({extname: 'hbs'}));
+app.set('view engine', "hbs")
+
+
+//routes
 app.get('/', (req, res) =>{
     res.send("Hello from express")
 });
