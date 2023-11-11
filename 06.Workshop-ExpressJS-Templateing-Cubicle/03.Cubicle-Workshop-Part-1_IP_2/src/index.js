06970f397d1bb7e5f64479a5211a976967e9580b
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-11-11 14:49:37 +0200

    extract routs

--- 06.Workshop-ExpressJS-Templateing-Cubicle/03.Cubicle-Workshop-Part-1_IP_2/src/index.js
+++ 06.Workshop-ExpressJS-Templateing-Cubicle/03.Cubicle-Workshop-Part-1_IP_2/src/index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const path = require('path');
 const expressConfig = require('./connfig/expressConfig');
 const handlebarsConfig = require('./connfig/handlebarsConfig');
-const homeController = require('./controllers/homeController')
-const cubeController = require('./controllers/cubeController')
+
+const routes = require('./routes')
+
 const app = express();
 const PORT = 5000;
 
@@ -13,14 +14,11 @@ const PORT = 5000;
 expressConfig(app)
 //require('./connfig/expressConfig')(app) - second variant
 handlebarsConfig(app)
+app.use(routes)
 
 // app.get('/', homeController.getHome) - not good practise
 
-app.use(homeController);
-app.use('/cubes',cubeController);
-app.get("*", (req, res) =>{
-    res.redirect('/404')
-})
+
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
