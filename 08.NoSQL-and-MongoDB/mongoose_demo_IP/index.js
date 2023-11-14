82cd8cf9add9e7dfc637f9ac3c82863a1fcdcd5c
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-11-14 19:12:10 +0200

    mongo DB demo update

--- 08.NoSQL-and-MongoDB/mongoose_demo_IP/index.js
+++ 08.NoSQL-and-MongoDB/mongoose_demo_IP/index.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const Dog = require('./models/Dog')
 
 async function connectDb (){
    await mongoose.connect('mongodb://127.0.0.1:27017/dogsDB');
 
    console.log("DB connected successfully")
 
+   const dogs = await Dog.find({color: "orange"})
 
+   dogs.forEach( dog => dog.greed())
 
 }
 connectDb()
--- 08.NoSQL-and-MongoDB/mongoose_demo_IP/models/Dog.js
+++ 08.NoSQL-and-MongoDB/mongoose_demo_IP/models/Dog.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose');
 
-const dogSchema = new mongoose.Schema()
+const dogSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require : true,
+    },
+    age: Number,
+    breed: String
+});
+
+dogSchema.methods.greed = function () {
+    console.log(`Hello I am Dog, and my name is ${this.name}`);
+}
+const Dog = mongoose.model("Dog", dogSchema);
+
+module.exports = Dog
