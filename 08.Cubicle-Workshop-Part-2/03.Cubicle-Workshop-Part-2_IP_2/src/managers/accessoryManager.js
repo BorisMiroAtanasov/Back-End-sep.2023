a2e2056e29f60809fea6487f85be220ed62e2043
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-11-19 02:21:27 +0200

    CUBE - 2 may - show remaining only

--- 08.Cubicle-Workshop-Part-2/03.Cubicle-Workshop-Part-2_IP_2/src/managers/accessoryManager.js
+++ 08.Cubicle-Workshop-Part-2/03.Cubicle-Workshop-Part-2_IP_2/src/managers/accessoryManager.js
@@ -4,3 +4,5 @@ const Accessory = require('../models/Accessory')
 exports.create = (accessoryData) => Accessory.create(accessoryData);
 
 exports.getAll = () => Accessory.find()
+
+exports.getOthers = (accessoriesIds) => Accessory.find({_id: {$nin: accessoriesIds}})
