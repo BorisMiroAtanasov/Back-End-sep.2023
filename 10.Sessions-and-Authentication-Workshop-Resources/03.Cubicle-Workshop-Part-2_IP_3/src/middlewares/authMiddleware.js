9660e88340104f170e75cf4e145cf1cc82eedbd8
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-11-26 01:38:13 +0200

    CUBE - 3 may 2023 - logout

--- 10.Sessions-and-Authentication-Workshop-Resources/03.Cubicle-Workshop-Part-2_IP_3/src/middlewares/authMiddleware.js
+++ 10.Sessions-and-Authentication-Workshop-Resources/03.Cubicle-Workshop-Part-2_IP_3/src/middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ exports.auth = async (req, res, next) => {
         try {
             const user = await jwt.verify(token, SECRET);
             req.user = user
-            res.locals = user
+            res.locals.user = user
             res.locals.isAuthenticate = true
             next()
 
