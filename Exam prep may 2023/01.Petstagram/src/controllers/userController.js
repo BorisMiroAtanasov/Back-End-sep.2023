beb315c5f62e9fe1626e8eae8cb92458a942d69b
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-12-03 00:51:50 +0200

    add register view

--- Exam prep may 2023/01.Petstagram/src/controllers/userController.js
+++ Exam prep may 2023/01.Petstagram/src/controllers/userController.js
@@ -3,6 +3,11 @@ const router = require('express').Router();
 
 router.get('/login', (req, res) =>{
     res.render('users/login')
+});
+
+
+router.get('/register', (req, res) =>{
+    res.render('users/register')
 })
 
 
