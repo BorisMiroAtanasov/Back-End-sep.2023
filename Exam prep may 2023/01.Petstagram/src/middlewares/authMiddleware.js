a3f6c1744b4a5b86b1788bbdce7014f9e286ae6a
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-12-03 18:29:56 +0200

    dinamic navigation - add conditionl to main layout and add locals

--- Exam prep may 2023/01.Petstagram/src/middlewares/authMiddleware.js
+++ Exam prep may 2023/01.Petstagram/src/middlewares/authMiddleware.js
@@ -7,7 +7,10 @@ exports.auth =async (req,res, next) =>{
     if(token){
         try {
            const decodedToken =  await jwt.verify(token, SECRET); // decodedToken = payload
-            req.user= decodedToken
+            req.user= decodedToken;
+            res.locals.user = decodedToken
+            res.locals.isAuthenticated = true
+
            next()
         } catch (err) {
 
