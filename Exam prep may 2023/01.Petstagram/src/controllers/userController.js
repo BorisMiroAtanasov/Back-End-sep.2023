6236854f5a76d3a6a74a5fd9f7eb0854974938e7
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-12-03 14:41:48 +0200

    modify login and register forms

--- Exam prep may 2023/01.Petstagram/src/controllers/userController.js
+++ Exam prep may 2023/01.Petstagram/src/controllers/userController.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
+const userManager = require ('../managers/userManager')
 
 
 router.get('/login', (req, res) =>{
     res.render('users/login')
 });
+
+router.post('/login', (req, res) =>{
+
+    
+});
+
 
 
 router.get('/register', (req, res) =>{
---
+++ Exam prep may 2023/01.Petstagram/src/managers/userManager.js
@@ -0,0 +1,11 @@
+const User = require('../models/User')
+
+exports.login = () =>{
+    
+};
+
+exports.register = () =>{
+    
+};
+
+
