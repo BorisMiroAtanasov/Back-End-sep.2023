96fc7aa200b9b0b72f7868a43839e5901383a2bd
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-12-03 18:21:05 +0200

    authentication middleware

--- Exam prep may 2023/01.Petstagram/src/controllers/homeController.js
+++ Exam prep may 2023/01.Petstagram/src/controllers/homeController.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-   console.log(req.user); 
-    res.render('home')
+   //console.log(req.user); // only for check do we have auth user in request
 });
 
 module.exports = router
