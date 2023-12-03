559b6d7cfddb17eac63f483b4cbf5b673b3623f5
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-12-03 16:00:50 +0200

    password hash

--- Exam prep may 2023/01.Petstagram/src/managers/userManager.js
+++ Exam prep may 2023/01.Petstagram/src/managers/userManager.js
@@ -1,11 +1,21 @@
 const User = require('../models/User')
 
-exports.login = () =>{
+exports.login = (username, password) =>{
+
+
     
 };
 
-exports.register = () =>{
+exports.register = async(userData) => {
+    const user = await User.findOne({username:userData.username }); // без await връща промис обект
+
+    if(user){
+        throw new Error ('Username already exists')
+    }
+
+    return User.create(userData);
+
+}
     
-};
 
 
