4deba6db4bd97f4529f8dadc124cf04d79ac77a0
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-12-04 23:13:15 +0200

    repair task

--- Exam prep may 2023/01.Petstagram/src/controllers/homeController.js
+++ Exam prep may 2023/01.Petstagram/src/controllers/homeController.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-   //console.log(req.user); // only for check do we have auth user in request
+   console.log(req.user); // only for check do we have auth user in request
+   res.render('home')
 });
 
 module.exports = router
--- Exam prep may 2023/01.Petstagram/src/middlewares/authMiddleware.js
+++ Exam prep may 2023/01.Petstagram/src/middlewares/authMiddleware.js
@@ -1,15 +1,15 @@
 const jwt = require('../lib/jwt')
 const {SECRET, TOKEN_KEY} = require('../config/config')
 
-exports.auth =async (req,res, next) =>{
+exports.auth = async (req,res, next) =>{
     const token = req.cookies[TOKEN_KEY] // има ли токен в cookies
 
     if(token){
         try {
            const decodedToken =  await jwt.verify(token, SECRET); // decodedToken = payload
             req.user= decodedToken;
-            res.locals.user = decodedToken
-            res.locals.isAuthenticated = true
+             res.locals.user = decodedToken;
+             res.locals.isAuthenticated = true;
 
            next()
         } catch (err) {
@@ -30,7 +30,7 @@ exports.auth =async (req,res, next) =>{
 
 exports.isAuth = (req,res, next) =>{
     if(!req.user){
-       return res.redirect('users/login')
+       return res.redirect('/users/login')
     }
 
     next()
