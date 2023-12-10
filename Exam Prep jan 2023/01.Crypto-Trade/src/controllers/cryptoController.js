22e0a9cebdab4d06a0ce95022b0b643c5648f9ef
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-12-10 14:54:08 +0200

    Crypto - edit , create , delete

--- Exam Prep jan 2023/01.Crypto-Trade/src/controllers/cryptoController.js
+++ Exam Prep jan 2023/01.Crypto-Trade/src/controllers/cryptoController.js
@@ -3,9 +3,9 @@ const cryptoManager = require('../managers/cryptoManager');
 const {getErrorMessage} = require('../utils/errorHelpers');
 const {isAuth} = require('../middlewares/authMiddleware');
 
-router.get('/catalog' ,(req,res) =>{
-    //const  cryptos = await cryptoManager.getAll().lean() // photoManager.getAll -> дава query, -> .lean() когато се ресолвне , ще даде чистия масив
-     res.render('crypto/catalog' ) //, {cryptos}
+router.get('/catalog' ,async (req,res) =>{
+    const  cryptos = await cryptoManager.getAll().lean() // photoManager.getAll -> дава query, -> .lean() когато се ресолвне , ще даде чистия масив
+     res.render('crypto/catalog' , {cryptos}) //, {cryptos}
  });
 
  router.get('/create' , (req, res) =>{
@@ -27,5 +27,56 @@ router.post('/create' , async(req, res) =>{
     }
     
 });
+
+router.get('/:cryptoId/details', async(req,res) =>{//async
+   const cryptoId = req.params.cryptoId;
+  // console.log(cryptoId);
+    const crypto = await cryptoManager.getOne(cryptoId).lean();//.populate('comments.user')
+    const isOwner = req.user?._id == crypto.owner._id;
+   // console.log(req.user._id);
+   // console.log(crypto.owner._id);
+    //console.log(photo)
+
+
+    res.render('crypto/details',{crypto, isOwner})
+    });
+
+    router.get('/:cryptoId/edit', async(req, res) =>{
+        const cryptoId = req.params.cryptoId;
+        const crypto = await cryptoManager.getOne(cryptoId).lean()
+
+
+        res.render('crypto/edit',{crypto})//{photo, isOwner}
+    });
+
+    router.get('/:cryptoId/delete' , async(req, res) =>{
+
+        const cryptoId = req.params.cryptoId;
+        await cryptoManager.delete(cryptoId);
+
+        res.redirect('/cryptos/catalog')
+
+
+    })
+
+    router.post('/:cryptoId/edit', async(req,res) =>{
+        const cryptoId = req.params.cryptoId;
+        const cryptoData = req.body;
+
+        
+        try {
+            await cryptoManager.edit(cryptoId,cryptoData)
+            res.redirect(`/cryptos/${cryptoId}/details`);
+            
+        } catch (err) {
+            res.render(`crypto/details`, {error: `Unabel to update photo`, ...cryptoData})
+            
+        }
+
+
+    });
+
+
+
 
  module.exports = router
