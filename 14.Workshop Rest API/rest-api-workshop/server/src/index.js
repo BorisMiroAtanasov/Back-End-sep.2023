c524515ef7649d53c15a725f0955b0d2f8ca6186
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-01-09 19:36:04 +0200

    RESTful api data

--- 14.Workshop Rest API/rest-api-workshop/server/src/index.js
+++ 14.Workshop Rest API/rest-api-workshop/server/src/index.js
@@ -9,7 +9,8 @@ const PORT = 3030;
 
 
 // middleware configuration
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false})) // ulrencodet, querystring
+app.use(express.json()) // разчита application json files -> от AJAX requests
 app.use(cors());
 // app.use((req, res, next) =>{
 //     res.setHeader('Access-Control-Allow-Origin', '*');
