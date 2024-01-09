593a52431616713bab811b1177dd16286e802461
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-01-09 19:27:43 +0200

    RESTFul api set cors

--- 14.Workshop Rest API/rest-api-workshop/server/src/index.js
+++ 14.Workshop Rest API/rest-api-workshop/server/src/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const cors = require('cors')
+
 const routes = require('./routes')
 
 
 const app = express();
 const PORT = 3030;
+
+
+// middleware configuration
+app.use(express.urlencoded({extended: false}))
+app.use(cors());
+// app.use((req, res, next) =>{
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', '*');
+//     res.setHeader('Access-Control-Allow-Headers', '*');
+
+//     next()
+// })
+
+
 
 console.log('works');
 
