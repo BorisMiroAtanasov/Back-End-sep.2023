1183594f62864419e23ccd450d7e99e84b3b324c
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-01-13 08:09:49 +0200

    RESTful API - edit functionality

--- 14.Workshop Rest API/rest-api-workshop/server/src/controllers/furnitureController.js
+++ 14.Workshop Rest API/rest-api-workshop/server/src/controllers/furnitureController.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-   // console.log(req.user)
+  // console.log(req.user)
   try {
     const { description, img, make, material, model, price, year } = req.body;
     await furnitureService.create({
@@ -23,12 +23,49 @@ router.post("/", async (req, res) => {
       model,
       price,
       year,
-      _ownerId:req.user._id,
+      _ownerId: req.user._id,
     });
     res.status(201).end();
   } catch ({ message }) {
     res.status(400).json({ message });
   }
 });
+
+router.get("/:furnitureId", async (req, res) => {
+    try {
+        const { furnitureId } = req.params;
+      
+        const furniture = await furnitureService.getOne(furnitureId);
+      
+        res.json(furniture);
+        
+    } catch ({ message }) {
+        res.status(400).json({ message });
+        
+    }
+});
+
+router.put("/:furnitureId", async(req, res) =>{
+    try {
+        const { furnitureId } = req.params;
+        const { description, img, make, material, model, price, year } = req.body;
+
+        const furnitureData = {
+            description,
+            img,
+            make,
+            material,
+            model,
+            price,
+            year,
+            _ownerId: req.user._id,
+          };
+
+        await furnitureService.updateOne(furnitureId,furnitureData)
+        res.status(200).end();
+    } catch ({ message }) {
+        res.status(400).json({ message });
+    }
+})
 
 module.exports = router;
--- 14.Workshop Rest API/rest-api-workshop/server/src/services/furnitureService.js
+++ 14.Workshop Rest API/rest-api-workshop/server/src/services/furnitureService.js
@@ -5,3 +5,9 @@ const Furniture = require('../models/Furnitures');
 exports.getAll = () => Furniture.find();
 
 exports.create = (furnitureData) => Furniture.create(furnitureData);
+
+
+exports.getOne = (furnitureId) =>Furniture.findById(furnitureId)
+
+
+exports.updateOne = (furnitureId, furnitureData) => Furniture.findByIdAndUpdate(furnitureId,furnitureData)
