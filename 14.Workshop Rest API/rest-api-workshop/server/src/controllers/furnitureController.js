1b317d63d6d27722f62e2b19a131c20857f56b71
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-01-13 17:24:55 +0200

    RESTful API My Furniture

--- 14.Workshop Rest API/rest-api-workshop/server/src/controllers/furnitureController.js
+++ 14.Workshop Rest API/rest-api-workshop/server/src/controllers/furnitureController.js
@@ -3,7 +3,8 @@ const furnitureService = require("../services/furnitureService");
 
 router.get("/", async (req, res) => {
   try {
-    const furnitures = await furnitureService.getAll();
+    const query = req.query;
+    const furnitures = await furnitureService.getAll(query);
 
     res.json(furnitures);
   } catch ({ message }) {
@@ -66,6 +67,19 @@ router.put("/:furnitureId", async(req, res) =>{
     } catch ({ message }) {
         res.status(400).json({ message });
     }
-})
+});
+
+router.delete("/:furnitureId", async(req,res) =>{
+    try {
+        const { furnitureId } = req.params;
+        await furnitureService.delete(furnitureId);
+        res.status(200).end();
+    } catch ({ message }) {
+        res.status(400).json({ message });
+        
+    }
+
+
+});
 
 module.exports = router;
--- 14.Workshop Rest API/rest-api-workshop/server/src/services/furnitureService.js
+++ 14.Workshop Rest API/rest-api-workshop/server/src/services/furnitureService.js
@@ -2,7 +2,19 @@ const Furniture = require('../models/Furnitures');
 
 
 
-exports.getAll = () => Furniture.find();
+exports.getAll = async(qs) => {
+    let query = Furniture.find();
+
+    if(qs.where){
+        let [_ , ownerId] = qs.where.split("=");
+        ownerId = ownerId.replaceAll('"', "");
+        //query = query.find({_ownerId:ownerId});
+        query = query.where("_ownerId").eq(ownerId)
+    };
+    const result = await query;
+
+    return result;
+}
 
 exports.create = (furnitureData) => Furniture.create(furnitureData);
 
@@ -10,4 +22,6 @@ exports.create = (furnitureData) => Furniture.create(furnitureData);
 exports.getOne = (furnitureId) =>Furniture.findById(furnitureId)
 
 
-exports.updateOne = (furnitureId, furnitureData) => Furniture.findByIdAndUpdate(furnitureId,furnitureData)
+exports.updateOne = (furnitureId, furnitureData) => Furniture.findByIdAndUpdate(furnitureId,furnitureData);
+
+exports.delete =  (furnitureId) => Furniture.findByIdAndDelete(furnitureId)
